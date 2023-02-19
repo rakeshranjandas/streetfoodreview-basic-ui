8159e4ac9e3c5ca7c7db5f9366e3b41692289fc4
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-02-19 21:32:02 +0530

    Implement Leaflet for maps

--- src/components/User/Display/Reviews/ReviewsViewMap.js
+++ src/components/User/Display/Reviews/ReviewsViewMap.js
@@ -1,5 +1,27 @@
 import React from "react"
+import "leaflet/dist/leaflet.css"
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import { Icon } from "leaflet"
+const myIcon = new Icon({
+  iconUrl: markerIconPng,
+  iconSize: [20, 32],
+})
 
 export default function ReviewsViewMap() {
-  return <div>ReviewsViewMap</div>
+  const position = [51.505, -0.09]
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={myIcon}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
 }
